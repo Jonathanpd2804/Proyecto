9fb5bb64f582745f70f8af66e291b186523aed63
Jonathanpd2804 <jonathanpd280403@gmail.com>
2023-04-18 18:03:49 +0100

    Cambio de nombre de archivo

--- lib/exports.dart
+++ lib/exports.dart
@@ -8,7 +8,7 @@ export 'package:david_perez/pages/login_page.dart';
 export 'package:david_perez/pages/register_page.dart';
 export 'package:david_perez/pages/forgot_pw_page.dart';
 export 'package:david_perez/pages/home_page.dart';
-export 'package:david_perez/pages/trabajadores_page.dart';
+export 'package:david_perez/pages/workers_page.dart';
 
     //Calendars
     export 'package:david_perez/pages/calendars/calendar_worker_page.dart';

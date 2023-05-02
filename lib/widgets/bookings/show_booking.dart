ebfac8c23635e238928893126beff27c5df14bec
Jonathanpd2804 <jonathanpd280403@gmail.com>
2023-05-02 19:25:24 +0100

    Ver productos perfil

--- lib/widgets/bookings/show_booking.dart
+++ lib/widgets/bookings/show_booking.dart
@@ -28,6 +28,173 @@ class ShowBookingDialogState extends State<ShowBookingDialog> {
     productController.text = widget.booking["productoId"];
   }
 
+  Future<Map<String, dynamic>?> getProductData(String productId) async {
+    final doc = await getProductById(productId);
+    if (doc.exists) {
+      return doc.data() as Map<String, dynamic>;
+    } else {
+      return null;
+    }
+  }
+
+  Future<DocumentSnapshot> getProductById(String productId) async {
+    final doc = await FirebaseFirestore.instance
+        .collection('productos')
+        .doc(productId)
+        .get();
+
+    return doc;
+  }
+
+  void _showFlipCardDialog(BuildContext context, Map<String, dynamic> product) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+            backgroundColor: Colors.transparent,
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Stack(alignment: Alignment.center, children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    height: 300,
+                    child: FlipCard(
+                      fill: Fill.fillBack,
+                      direction: FlipDirection.HORIZONTAL,
+                      side: CardSide.FRONT,
+                      front: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 8,
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                              ),
+                              child: Stack(children: [
+                                Image.network(
+                                  product['ImagenURL'],
+                                  fit: BoxFit.cover,
+                                  width: 200,
+                                  height: 225,
+                                ),
+                              ]),
+                            ),
+                            const Padding(
+                                padding: EdgeInsets.all(18.0),
+                                child: Icon(Icons.touch_app))
+                          ],
+                        ),
+                      ),
+                      back: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 8,
+                        child: Column(children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Scrollbar(
+                                    child: SizedBox(
+                                      height: 45, // Altura del contenedor
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: SingleChildScrollView(
+                                          child: Text(
+                                            product["Título"],
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 5),
+                                    child: Divider(
+                                        thickness: 1, color: Colors.black87),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 20.0),
+                                    child: Scrollbar(
+                                      child: SizedBox(
+                                        height: 90, // Altura del contenedor
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 8.0),
+                                          child: SingleChildScrollView(
+                                            child: Text(
+                                              product["Descripción"],
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 15,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 20.0),
+                                    child: Scrollbar(
+                                      child: SizedBox(
+                                        height: 20, // Altura del contenedor
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 8.0),
+                                          child: SingleChildScrollView(
+                                            child: Text(
+                                              product["Precio"],
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 15,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(18.0),
+                            child: Icon(Icons.touch_app),
+                          )
+                        ]),
+                      ),
+                    ),
+                  )
+                ])));
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -68,10 +235,19 @@ class ShowBookingDialogState extends State<ShowBookingDialog> {
       actions: [
         TextButton(
           onPressed: () {
+            getProductData(productController.text).then((product) {
+              _showFlipCardDialog(context, product!);
+            });
+          },
+          child: const Text('Ver producto'),
+        ),
+        TextButton(
+          onPressed: () {
             Navigator.pop(context);
           },
           child: const Text('Cancelar'),
         ),
+        
       ],
     );
   }
